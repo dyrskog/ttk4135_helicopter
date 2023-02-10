18d585385f18902a0596fb2691e866e67c8eb380
Jørgen Dyrskog <joergen.dyrskog@gmail.com>
2023-02-10 13:42:31 +0100

    Add lab 2 for reals this time

---
+++ lab2/plot_simout.m
@@ -0,0 +1,23 @@
+tempstruct = load('simout');
+simout = tempstruct.simout;
+
+u = simout(:,1);
+x1 = simout(:,2);
+x2 = simout(:,3);
+
+figure(2)
+subplot(511)
+stairs(t,u),grid
+ylabel('u')
+subplot(512)
+plot(t,x1,'m',t,x1,'mo'),grid
+ylabel('lambda')
+subplot(513)
+plot(t,x2,'m',t,x2','mo'),grid
+ylabel('r')
+subplot(514)
+plot(t,x3,'m',t,x3,'mo'),grid
+ylabel('p')
+subplot(515)
+plot(t,x4,'m',t,x4','mo'),grid
+xlabel('tid (s)'),ylabel('pdot')
